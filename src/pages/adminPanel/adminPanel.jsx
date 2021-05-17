3554e07062e17d91463db211dff0284c2fcb3247
Masnc7 <masnc7@gmail.com>
2021-05-17 17:27:49 -0300

    Init

---
+++ src/pages/adminPanel/adminPanel.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function adminPanel() {
+  return <div></div>;
+}
+
+export default adminPanel;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Holis</h1>
+      <img src="https://api.carlosgutierrez.com.uy/fotos/fotos/10735/ppal-desktop-1x.jpg"></img>
+    </div>
+  );
+}
+
+export default Home;
