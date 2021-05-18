68ebe2be029bb07f0c7a7f5b33637082302d15ba
marvlasa <martin.vlasa@gmail.com>
2021-05-17 21:33:10 -0300

    add product card with tv img, name, descrption and button

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from "../../components/footer/Footer";
+import Product from "../product/Product";
 
 function Home() {
   return (
     <div>
+      <Product />
       <Footer />
     </div>
   );
