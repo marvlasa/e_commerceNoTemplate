f6505c1d5c1f1344c8485b0cb6830839f1d11b53
marvlasa <martin.vlasa@gmail.com>
2021-05-18 13:17:47 -0300

    eso

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Footer from "../../components/footer/Footer";
+import Product from "../product/Product";
 
 import Carrousel from "../../components/carrousel/Carrousel";
 import NavBar from "../../components/navbar/Navbar";
 
-
 function Home() {
   return (
     <div>
+      <Carrousel />
+      <Product />
 
       <Footer />
-
-      <Carrousel />
-
     </div>
   );
 }
--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "./Product.css";
 
 function Product() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="row align-items-center mb-5">
+        <div className="col-md-3 pricing rounded">
+          <img
+            src="https://www.estrategiaynegocios.net/csp/mediapool/sites/dt.common.streams.StreamServer.cls?STREAMOID=RDGJtT2_wvbyq5AjPJET4c$daE2N3K4ZzOUsqbU5sYsJwndwh_25dutpI2C1IJwl6FB40xiOfUoExWL3M40tfzssyZqpeG_J0TFo7ZhRaDiHC9oxmioMlYVJD0A$3RbIiibgT65kY_CSDiCiUzvHvODrHApbd6ry6YGl5GGOZrs-&CONTENTTYPE=image/jpeg"
+            alt=""
+          />
+          <h1>Smart TV Samsung 75 2020 QLED 4K HDR</h1>
+          <p>
+            Los colores son tan reales como deberían ser. Con la exclusiva
+            tecnología de punto cuántico de Samsung, QLED aumenta el volumen de
+            color en un 100 %...
+          </p>
+          <p>Precio: $20,2500</p>
+          <button className="btn btn-warning mb-3">Agregar al carrito</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
