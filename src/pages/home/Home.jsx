d451de37098ccb8ad05cacaf5e800ea71a424876
marvlasa <martin.vlasa@gmail.com>
2021-05-18 14:52:16 -0300

    add footer and navbar to app

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,16 @@
 import React from "react";
-import Footer from "../../components/footer/Footer";
+// import Footer from "../../components/footer/Footer";
 import Product from "../product/Product";
 
 import Carrousel from "../../components/carrousel/Carrousel";
 
-import Navbar from "../../components/navbar/Navbar";
+// import Navbar from "../../components/navbar/Navbar";
 
 function Home() {
   return (
     <div>
-      <Navbar />
       <Carrousel />
       <Product />
-
-      <Footer />
     </div>
   );
 }
