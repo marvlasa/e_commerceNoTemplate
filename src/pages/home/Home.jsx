1e06a775df9176a80a9235f765b563897c0879fc
Masnc7 <masnc7@gmail.com>
2021-05-18 13:10:45 -0300

    Changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,6 @@ import Footer from "../../components/footer/Footer";
 import Product from "../product/Product";
 
 import Carrousel from "../../components/carrousel/Carrousel";
-import NavBar from "../../components/navbar/Navbar";
 
 function Home() {
   return (
