ec3ef6d5df5e7aa1ea3a2e7f592695b88682dbb0
Masnc7 <masnc7@gmail.com>
2021-05-19 18:37:07 -0300

    Scroll

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect } from "react";
 // import Footer from "../../components/footer/Footer";
 import Product from "../product/Product";
 
@@ -7,6 +7,9 @@ import Carrousel from "../../components/carrousel/Carrousel";
 // import Navbar from "../../components/navbar/Navbar";
 
 function Home({ handleCartItems }) {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div>
       <Carrousel />
